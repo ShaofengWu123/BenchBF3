a67473566eb96b2277641aef9723e3b12f0e5b6a
ShaofengWu123 <wsf123@link.cuhk.edu.hk>
2025-12-02 05:56:04 +0000

    change mac

--- flexio/dpa_send_mt/common/dpa_send_common.h
+++ flexio/dpa_send_mt/common/dpa_send_common.h
@@ -8,9 +8,9 @@
 
 #define LOG_WQ_DATA_ENTRY_BSIZE 10 /* WQ buffer logarithmic size */
 
-__attribute__((unused)) static struct ether_addr DST_ADDR = { {0xA0, 0x88, 0xC2, 0x32, 0x04, 0x30} };
+__attribute__((unused)) static struct ether_addr DST_ADDR = { {0x8c, 0x91, 0x3a, 0xdf, 0xc8, 0x6e} };
 
 // bf2 mac addr
-__attribute__((unused)) static struct ether_addr SRC_ADDR = { {0x02, 0x01, 0x01, 0x01, 0x01, 0x01} };
+__attribute__((unused)) static struct ether_addr SRC_ADDR = { {0x8c, 0x91, 0x3a, 0xdf, 0xc9, 0x9e} };
 
 __attribute__((unused)) static struct ether_addr DST_DPA_ADDR = { {0x01, 0x01, 0x01, 0x01, 0x01, 0x01} };
