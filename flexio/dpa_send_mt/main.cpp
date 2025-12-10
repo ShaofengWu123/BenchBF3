30eb54fbfc339564bb47a69a9142f4d80c7eecc6
ShaofengWu123 <wsf123@link.cuhk.edu.hk>
2025-12-10 14:47:39 +0000

    update

--- flexio/dpa_send_mt/main.cpp
+++ flexio/dpa_send_mt/main.cpp
@@ -115,10 +115,12 @@ int main(int argc, char **argv) {
     //DOCA_LOG_INFO("RPC call finished");
 
     // Call the activation RPC for each thread
+    uint32_t activation_id;
     for (size_t i = 0; i < FLAGS_g_thread_num; i++) {
         DOCA_LOG_INFO("Activating thread %d", i + FLAGS_begin_thread);
-        Assert(flexio_process_call(global_ctx->get_process(), &dpa_send_mt_activate_handler, &rpc_ret_val, 
-            event_handlers[i]) == FLEXIO_STATUS_SUCCESS);
+        // Retrieve the activation ID for the event handler
+        activation_id = flexio_event_handler_get_activation_id(event_handlers[i]);
+        Assert(flexio_process_call(global_ctx->get_process(), &dpa_send_mt_activate_handler, &rpc_ret_val, activation_id) == FLEXIO_STATUS_SUCCESS);
     }
     DOCA_LOG_INFO("Activation finished");
 
