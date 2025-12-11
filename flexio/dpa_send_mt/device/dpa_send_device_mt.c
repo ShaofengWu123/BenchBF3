7e8c86a5a53c26373f44662749c660f43c6ce881
ShaofengWu123 <wsf123@link.cuhk.edu.hk>
2025-12-11 03:00:05 +0000

    test

--- flexio/dpa_send_mt/device/dpa_send_device_mt.c
+++ flexio/dpa_send_mt/device/dpa_send_device_mt.c
@@ -22,9 +22,9 @@ flexio_dev_rpc_handler_t dpa_send_mt_activate_handler;
 flexio_dev_event_handler_t dpa_send_mt_device_event_handler; /* Event handler function */
 
 #define hdr_size 42
-// #define data_size 22
+#define data_size 22
 // #define data_size 86
-#define data_size 214
+// #define data_size 214
 #define pkt_size (hdr_size + data_size)
 
 #define MAX_THREADS 190
